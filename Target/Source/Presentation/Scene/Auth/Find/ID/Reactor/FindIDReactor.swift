e63845b1251e71e84d6b0e80b26b258a9371fe00
김상은 <81676542+SangAu124@users.noreply.github.com>
2022-02-07 20:52:26 +0900

    ✨ :: 아이디 찾기 Reactor파일추가

---
+++ Target/Source/Presentation/Scene/Auth/Find/ID/Reactor/FindIDReactor.swift
@@ -0,0 +1,39 @@
+//
+//  FindIDReactor.swift
+//  MOIZA
+//
+//  Created by 김상금 on 2022/02/07.
+//  Copyright © 2022 com.connect. All rights reserved.
+//
+
+import Foundation
+import ReactorKit
+import RxFlow
+import RxCocoa
+
+final class FindIDReactor: Reactor, Stepper{
+    // MARK: - Properties
+    var steps: PublishRelay<Step> = .init()
+    
+    private let disposeBag = DisposeBag()
+    
+    // MARK: - Reactor
+    enum Action {}
+    enum Mutation {}
+    struct State {}
+    
+    let initialState: State
+    
+    // MARK: - Init
+    init() {
+        initialState = State()
+    }
+    
+}
+
+// MARK: - Mutate
+extension FindIDReactor {
+    func mutate(action: Action) -> Observable<Mutation> {
+        
+    }
+}
