ad36ecbdc450a04487c5e216ace130bf0e3ea270
baegteun <baegteun@gmail.com>
2022-02-15 00:42:30 +0900

    :sparkles: :: PostList

---
+++ Target/Source/Presentation/Scene/Main/Items/PostList/PostList/Reactor/PostListReactor.swift
@@ -0,0 +1,65 @@
+//
+//  PostListReactor.swift
+//  MOIZA
+//
+//  Created by 최형우 on 2022/02/15.
+//  Copyright © 2022 com.connect. All rights reserved.
+//
+
+import ReactorKit
+import RxFlow
+import RxSwift
+import RxRelay
+
+final class PostListReactor: Reactor, Stepper {
+    // MARK: - Properties
+    var steps: PublishRelay<Step> = .init()
+    
+    private let disposeBag: DisposeBag = .init()
+    
+    // MARK: - Reactor
+    enum Action {
+        
+    }
+    enum Mutation {
+        
+    }
+    struct State {
+        
+    }
+    let initialState: State
+    
+    // MARK: - Init
+    init() {
+        initialState = State()
+    }
+    
+}
+
+// MARK: - Mutate
+extension PostListReactor {
+    func mutate(action: Action) -> Observable<Mutation> {
+        switch action {
+            
+        }
+        return .empty()
+    }
+}
+
+// MARK: - Reduce
+extension PostListReactor {
+    func reduce(state: State, mutation: Mutation) -> State {
+        var newState = state
+        
+        switch mutation {
+            
+        }
+        
+        return newState
+    }
+}
+
+// MARK: - Method
+private extension PostListReactor {
+    
+}
