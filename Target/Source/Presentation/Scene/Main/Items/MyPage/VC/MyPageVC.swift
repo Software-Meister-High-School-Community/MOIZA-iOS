4f88cef2d8c1666eed49a0d4b2826908a96df94c
baegteun <baegteun@gmail.com>
2022-02-15 00:29:11 +0900

    :sparkles: :: MyPage

---
+++ Target/Source/Presentation/Scene/Main/Items/MyPage/VC/MyPageVC.swift
@@ -0,0 +1,11 @@
+//
+//  MyPageVC.swift
+//  MOIZA
+//
+//  Created by 최형우 on 2022/02/15.
+//  Copyright © 2022 com.connect. All rights reserved.
+//
+
+final class MyPageVC: baseVC<MyPageReactor> {
+    
+}
