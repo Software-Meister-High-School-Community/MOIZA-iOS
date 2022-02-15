d098187d53d6102704c047e347961ff24949cf60
김상은 <81676542+SangAu124@users.noreply.github.com>
2022-02-15 16:43:55 +0900

    📝:: makeFile

---
+++ Target/Source/Presentation/Scene/Auth/Find/FindPW/SendCert/Reactor/SendCertReactor.swift
@@ -0,0 +1,9 @@
+//
+//  SendCertReactor.swift
+//  MOIZA
+//
+//  Created by 김상금 on 2022/02/15.
+//  Copyright © 2022 com.connect. All rights reserved.
+//
+
+import Foundation
---
+++ Target/Source/Presentation/Scene/Auth/Find/FindPW/SendCert/VC/SendCertVC.swift
@@ -0,0 +1,9 @@
+//
+//  SendCertVC.swift
+//  MOIZA
+//
+//  Created by 김상금 on 2022/02/15.
+//  Copyright © 2022 com.connect. All rights reserved.
+//
+
+import Foundation
