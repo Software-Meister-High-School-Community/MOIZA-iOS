aa4e4efbbea866aed314b3afceb91a298c975ae5
김상은 <81676542+SangAu124@users.noreply.github.com>
2022-02-15 17:35:21 +0900

    ✨:: CheckID

--- Target/Source/Presentation/Scene/Auth/Find/FindPW/CheckID/Reactor/CheckIDReactor.swift
+++ Target/Source/Presentation/Scene/Auth/Find/FindPW/CheckID/Reactor/CheckIDReactor.swift
@@ -7,3 +7,46 @@
 //
 
 import Foundation
+import ReactorKit
+import RxFlow
+import RxCocoa
+import UIKit
+
+final class CheckIDReactor: Stepper, Reactor {
+    // MARK: - Properties
+    var steps: PublishRelay<Step> = .init()
+    
+    private let disposeBag = DisposeBag()
+    
+    // MARK: - Reactor
+    enum Action {
+        case nextButtonDidTap
+    }
+    enum Mtation {}
+    struct State {}
+    
+    let initialState: State
+    
+    // MARK: - Init
+    init() {
+        initialState = State()
+    }
+}
+
+// MARK: - Mutate
+extension CheckIDReactor {
+    func mutate(action: Action) -> Observable<Action> {
+        switch action {
+        case .nextButtonDidTap:
+            return nextButtonDidTap()
+        }
+    }
+}
+
+// MARK: - Method
+private extension CheckIDReactor {
+    func nextButtonDidTap() -> Observable<Mutation> {
+        steps.accept(MoizaStep.sendCertRequired)
+        return .empty()
+    }
+}
--- Target/Source/Presentation/Scene/Auth/Find/FindPW/CheckID/VC/CheckIDVC.swift
+++ Target/Source/Presentation/Scene/Auth/Find/FindPW/CheckID/VC/CheckIDVC.swift
@@ -6,4 +6,54 @@
 //  Copyright © 2022 com.connect. All rights reserved.
 //
 
-import Foundation
+import Then
+import RxFlow
+import RxCocoa
+import ReactorKit
+import FlexLayout
+import PinLayout
+import UIKit
+import Hero
+
+final class CheckIDVC: baseVC<CheckIDReactor> {
+    // MARK: Properties
+    private let rootContainer = UIView()
+    private let titleLabel = UILabel().then {
+        $0.font = UIFont(font: MOIZAFontFamily.Roboto.regular, size: 18)
+        $0.textColor = .label
+        $0.text = "아이디를 입력해주세요."
+        $0.numberOfLines = 1
+    }
+    private let idTextField = SignUpTextField().then {
+        $0.placeholder = "아이디"
+        $0.leftSpace(14)
+    }
+    private let nextButton = NextButton(title: "다음 단계")
+    // MARK: - UI
+    override func addView() {
+        view.addSubview(rootContainer)
+    }
+    override func setLayoutSubViews() {
+        rootContainer.pin.all(view.pin.safeArea)
+        rootContainer.flex.layout()
+    }
+    override func setLayout() {
+        rootContainer.flex.marginHorizontal(16).define { flex in
+            flex.addItem(titleLabel).height(23).marginTop(38)
+            // MARK: CERT
+            flex.addItem(idTextField).marginTop(30).height(40)
+            // MARK: Next
+            flex.addItem(nextButton).marginTop(127).width(88).height(36).alignSelf(.end)
+        }
+    }
+    override func bindView(reactor: CheckIDReactor) {
+        nextButton.rx.tap
+            .map { _ in Reactor.Action.nextButtonDidTap }
+            .bind(to: reactor.action)
+            .disposed(by: disposeBag)
+    }
+    override func configureNavigation() {
+        self.navigationItem.configAuthNavigation(title: "비밀번호 찾기")
+        self.navigationItem.configBack()
+    }
+}
