b53c9a6bf7cac0e94b8ad31952640fe31fd73382
pasin7793 <pasin7793@gmail.com>
2022-02-18 23:52:14 +0900

    :sparkles: PostBoard Reactor

---
+++ Target/Source/Presentation/Scene/Main/PostBoard/Model/PostType.swift
@@ -0,0 +1,12 @@
+//
+//  PostType.swift
+//  MOIZA
+//
+//  Created by 임준화 on 2022/02/18.
+//  Copyright © 2022 com.connect. All rights reserved.
+//
+
+enum PostType: String, Codable{
+    case question = "질문"
+    case normal = "일반"
+}
---
+++ Target/Source/Presentation/Scene/Main/PostBoard/PostBoardVC.swift
@@ -0,0 +1,17 @@
+//
+//  PostBoardVC.swift
+//  MOIZA
+//
+//  Created by 임준화 on 2022/02/18.
+//  Copyright © 2022 com.connect. All rights reserved.
+//
+
+import UIKit
+import Hero
+import M13Checkbox
+import RxDataSources
+import RxSwift
+import RxViewController
+import PinLayout
+import FlexLayout
+
