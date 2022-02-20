d9162926e4050b42de64ae0388b0b96491378bf8
baegteun <baegteun@gmail.com>
2022-02-20 17:03:10 +0900

    :sparkles: :: PostType

---
+++ Target/Source/Domain/Entities/PostType.swift
@@ -0,0 +1,12 @@
+//
+//  PostType.swift
+//  MOIZA
+//
+//  Created by 최형우 on 2022/02/20.
+//  Copyright © 2022 com.connect. All rights reserved.
+//
+
+enum PostType {
+    case normal
+    case question
+}
