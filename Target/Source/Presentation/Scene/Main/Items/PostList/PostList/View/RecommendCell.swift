1f3617c9d20604f25424ef882eb2fe4b4c27a27b
baegteun <baegteun@gmail.com>
2022-02-23 04:30:39 +0900

    :sparkles: :: RecommendCell

--- Target/Source/Presentation/Scene/Main/Items/PostList/PostList/View/RecommendCell.swift
+++ Target/Source/Presentation/Scene/Main/Items/PostList/PostList/View/RecommendCell.swift
@@ -1,3 +1,36 @@
 import UIKit
+import FlexLayout
 
-
+final class RecommendCell: baseCollectionViewCell<PostList> {
+    // MARK: - Properties
+    private let iconImageView = UIImageView()
+    private let typeLabel = UILabel().then {
+        $0.textColor = MOIZAAsset.moizaGray6.color
+        $0.font = UIFont(font: MOIZAFontFamily.Roboto.bold, size: 20)
+    }
+    private let titleLabel = UILabel().then {
+        $0.textColor = MOIZAAsset.moizaGray6.color
+        $0.font = UIFont(font: MOIZAFontFamily.Roboto.regular, size: 14)
+    }
+    
+    // MARK: - UI
+    override func setLayoutSubviews() {
+        contentView.flex.layout()
+    }
+    override func setLayout() {
+        contentView.flex.padding(20).define { flex in
+            flex.addItem(iconImageView).size(30).shrink(2)
+            flex.addItem(typeLabel).shrink(1)
+            flex.addItem(titleLabel).shrink(1)
+        }
+    }
+    override func configureCell() {
+        self.layer.cornerRadius = 20
+    }
+    
+    override func bind(_ model: PostList) {
+        iconImageView.image = model.type == .question ? MOIZAAsset.moizaQuestion.image : MOIZAAsset.moizaIdea.image
+        typeLabel.text = model.type == .question ? "오늘의 질문" : "오늘의 꿀팁"
+        titleLabel.text = model.title
+    }
+}
