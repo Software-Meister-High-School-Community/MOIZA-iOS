3c00994fad0d1412af7e539b7f0ebfe53f1b6eeb
baegteun <baegteun@gmail.com>
2022-02-25 05:30:45 +0900

    :sparkles: :: postListReactor

--- Target/Source/Presentation/Scene/Main/Items/PostList/PostList/Reactor/PostListReactor.swift
+++ Target/Source/Presentation/Scene/Main/Items/PostList/PostList/Reactor/PostListReactor.swift
@@ -20,16 +20,19 @@ final class PostListReactor: Reactor, Stepper {
     // MARK: - Reactor
     enum Action {
         case viewDidLoad
+        case viewWillAppear
         case majorButtonDidTap
         case majorDidChange(Major)
     }
     enum Mutation {
         case setMajor(Major)
         case setRecommendPostList([PostList])
+        case setPostList([PostList])
     }
     struct State {
         var major: Major
-        var recommendItem: [PostList]
+        var recommendItems: [PostList]
+        var postItems: [PostList]
     }
     let initialState: State
     
@@ -37,7 +40,8 @@ final class PostListReactor: Reactor, Stepper {
     init() {
         initialState = State(
             major: UserDefaultsLocal.shared.major,
-            recommendItem: []
+            recommendItems: [],
+            postItems: []
         )
     }
     
@@ -53,6 +57,8 @@ extension PostListReactor {
             return viewDidLoad()
         case let .majorDidChange(major):
             return .just(.setMajor(major))
+        case .viewWillAppear:
+            return viewWillAppear()
         }
         return .empty()
     }
@@ -67,7 +73,9 @@ extension PostListReactor {
         case let .setMajor(major):
             newState.major = major
         case let .setRecommendPostList(rec):
-            newState.recommendItem = rec
+            newState.recommendItems = rec
+        case let .setPostList(posts):
+            newState.postItems = posts
         }
         
         return newState
@@ -76,13 +84,45 @@ extension PostListReactor {
 
 // MARK: - Method
 private extension PostListReactor {
-    func viewDidLoad() -> Observable<Mutation> {
+    func viewWillAppear() -> Observable<Mutation> {
         let recommend: [PostList] = [
-            .init(title: "앱 아이콘 만드는 법", type: .question, commentCount: 2, likeCount: 3),
-            .init(title: "일러스트에서 아주 그냥 asdfasdfasdf", type: .normal, commentCount: 3, likeCount: 3)
-        ]
+            .init(title: "앱 아이콘 만드는 법", type: .question, commentCount: 2, likeCount: 3, liked: false),
+            .init(title: "일러스트에서 아주 그냥 asdfasdfasdf", type: .normal, commentCount: 3, likeCount: 3, liked: true),
+            .init(title: "앱 아이콘 만드는 법", type: .question, commentCount: 2, likeCount: 3, liked: false),
+            .init(title: "일러스트에서 아주 그냥 asdfasdfasdf", type: .normal, commentCount: 3, likeCount: 3, liked: true),
+            .init(title: "앱 아이콘 만드는 법", type: .question, commentCount: 2, likeCount: 3, liked: false),
+            .init(title: "일러스트에서 아주 그냥 asdfasdfasdf", type: .normal, commentCount: 3, likeCount: 3, liked: true)
+        ].filter {
+            if UserDefaultsLocal.shared.post == .all { return true }
+            return $0.type == UserDefaultsLocal.shared.post
+        }
+        let posts: [PostList] = [
+            .init(title: "대충제목대충제목대충제목대충제목대충제목", type: .question, commentCount: 13, likeCount: 163, liked: false),
+            .init(title: "ㅁㄴㅇㅇ", type: .normal, commentCount: 2, likeCount: 1, liked: true),
+            .init(title: "ㅁㄴㅇㅇ", type: .normal, commentCount: 2, likeCount: 1, liked: true),
+            .init(title: "ㅁㄴㅇㅇ", type: .normal, commentCount: 2, likeCount: 1, liked: false),
+            .init(title: "대충제목대충제목대충제목대충제목대충제목", type: .question, commentCount: 13, likeCount: 163, liked: false),
+            .init(title: "ㅁㄴㅇㅇ", type: .normal, commentCount: 2, likeCount: 1, liked: true),
+            .init(title: "ㅁㄴㅇㅇ", type: .question, commentCount: 43, likeCount: 1, liked: true),
+            .init(title: "ㅁㄴㅇㅇ", type: .normal, commentCount: 2, likeCount: 1, liked: false),
+            .init(title: "대충제목대충제목대충제목대충제목대충제목", type: .question, commentCount: 13, likeCount: 163, liked: false),
+            .init(title: "ㅁㄴㅇㅇ", type: .normal, commentCount: 2, likeCount: 1, liked: true),
+            .init(title: "ㅁㄴㅇㅇ", type: .normal, commentCount: 2, likeCount: 1, liked: true),
+            .init(title: "ㅁㄴㅇㅇ", type: .normal, commentCount: 2, likeCount: 1, liked: false),
+            .init(title: "대충제목대충제목대충제목대충제목대충제목", type: .question, commentCount: 13, likeCount: 163, liked: false),
+            .init(title: "ㅁㄴㅇㅇ", type: .normal, commentCount: 2, likeCount: 1, liked: true),
+            .init(title: "ㅁㄴㅇㅇ", type: .question, commentCount: 43, likeCount: 1, liked: true),
+            .init(title: "ㅁㄴㅇㅇ", type: .normal, commentCount: 2, likeCount: 1, liked: false)
+        ].filter {
+            if UserDefaultsLocal.shared.post == .all { return true }
+            return $0.type == UserDefaultsLocal.shared.post
+        }
         return .concat([
-            .just(.setRecommendPostList(recommend))
+            .just(.setRecommendPostList(recommend)),
+            .just(.setPostList(posts))
         ])
+    }
+    func viewDidLoad() -> Observable<Mutation> {
+        return .empty()
     }
 }
