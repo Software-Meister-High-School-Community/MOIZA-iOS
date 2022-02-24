5eff9c6dec58ba6ef8d836e3ec292b5e7f127d2f
baegteun <baegteun@gmail.com>
2022-02-25 05:31:52 +0900

    :sparkles: :: postCell

---
+++ Target/Source/Presentation/Scene/Main/Items/PostList/PostList/View/PostCell.swift
@@ -0,0 +1,75 @@
+import UIKit
+import Then
+import FlexLayout
+import RxSwift
+
+final class PostCell: baseTableViewCell<PostList> {
+    // MARK: - Properties
+    private let iconImageView = UIImageView()
+    private let titleLabel = UILabel().then {
+        $0.textColor = MOIZAAsset.moizaGray6.color
+        $0.font = UIFont(font: MOIZAFontFamily.Roboto.regular, size: 14)
+    }
+    private let commentCount = UIButton().then {
+        $0.setImage(.init(systemName: "bubble.right")?.tintColor(MOIZAAsset.moizaGray4.color), for: .normal)
+        $0.setTitleColor(MOIZAAsset.moizaGray5.color, for: .normal)
+        $0.titleLabel?.font = UIFont(font: MOIZAFontFamily.Roboto.regular, size: 12)
+    }
+    private let likeCount = UIButton().then {
+        $0.setTitleColor(MOIZAAsset.moizaGray5.color, for: .normal)
+        $0.titleLabel?.font = UIFont(font: MOIZAFontFamily.Roboto.regular, size: 12)
+    }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        model = nil
+        iconImageView.image = nil
+        titleLabel.text = nil
+        commentCount.setTitle("", for: .normal)
+        likeCount.setTitle("", for: .normal)
+        commentCount.imageView?.image = nil
+        likeCount.imageView?.image = nil
+    }
+    // MARK: - UI
+    override func setLayoutSubviews() {
+        contentView.flex.layout()
+    }
+    override func setLayout() {
+        contentView.flex.direction(.row).marginVertical(5).alignItems(.center).define { flex in
+            flex.addItem(iconImageView).marginLeft(10).size(25)
+            flex.addItem(titleLabel).width(52.4%).marginLeft(12)
+            flex.addItem().direction(.row).paddingLeft(15).grow(1).define { flex in
+                flex.addItem().shrink(1).grow(1).alignItems(.start).define { flex in
+                    flex.addItem(commentCount)
+                }
+                flex.addItem().shrink(1).grow(1).alignItems(.start).define { flex in
+                    flex.addItem(likeCount)
+                }
+            }
+        }
+    }
+    override func bind(_ model: PostList) {
+        iconImageView.image = model.type == .question ?
+        .init(systemName: "questionmark.circle.fill")?.tintColor(MOIZAAsset.moizaPrimaryYellow.color) :
+        MOIZAAsset.moizaBookReader.image.tintColor(MOIZAAsset.moizaPrimaryYellow.color)
+        titleLabel.text = model.title
+        commentCount.setTitle("\(model.commentCount)", for: .normal)
+        likeCount.setTitle("\(model.likeCount)", for: .normal)
+        if model.liked {
+            likeCount.setImage(.init(systemName: "heart.fill")?.tintColor(MOIZAAsset.moizaPrimaryYellow.color), for: .normal)
+        } else {
+            likeCount.setImage(.init(systemName: "heart")?.tintColor(MOIZAAsset.moizaGray4.color), for: .normal)
+        }
+    }
+    override func configureCell() {
+        self.contentView.backgroundColor = MOIZAAsset.moizaConstGray1.color
+        contentView.layer.cornerRadius = 5
+        self.selectionStyle = .none
+    }
+    override func darkConfigure() {
+        self.contentView.backgroundColor = MOIZAAsset.moizaDark2.color
+    }
+}
