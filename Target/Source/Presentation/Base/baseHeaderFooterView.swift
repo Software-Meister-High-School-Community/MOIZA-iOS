c9daced43e4f2b81fd6b97cb71fe1b8688f0de5b
baegteun <baegteun@gmail.com>
2022-02-26 19:00:36 +0900

    :sparkles: :: baseHeaderFooterView

--- Target/Source/Presentation/Base/baseHeaderFooterView.swift
+++ Target/Source/Presentation/Base/baseHeaderFooterView.swift
@@ -1,9 +1,29 @@
-//
-//  baseHeaderFooterView.swift
-//  MOIZA
-//
-//  Created by 최형우 on 2022/02/25.
-//  Copyright © 2022 com.connect. All rights reserved.
-//
+import UIKit
 
-import Foundation
+class baseHeaderFooterView<T>: UITableViewHeaderFooterView {
+    override init(reuseIdentifier: String?) {
+        super.init(reuseIdentifier: reuseIdentifier)
+        addView()
+        setLayout()
+        configureCell()
+        if traitCollection.userInterfaceStyle == .dark { darkConfigure() }
+    }
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        setLayoutSubviews()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+    
+    func addView(){}
+    func setLayout(){}
+    func setLayoutSubviews(){}
+    func configureCell(){}
+    var model: T? {
+        didSet { if let model = model { bind(model) } }
+    }
+    func darkConfigure(){}
+    func bind(_ model: T){}
+}
