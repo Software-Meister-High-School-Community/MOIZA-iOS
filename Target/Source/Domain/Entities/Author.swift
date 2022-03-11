10c54dc832203a9abb064d1abad916bfe0894d20
baegteun <baegteun@gmail.com>
2022-03-11 13:03:55 +0900

    :sparkles: :: Post Entity

---
+++ Target/Source/Domain/Entities/Author.swift
@@ -0,0 +1,6 @@
+struct Author {
+    let id: Int
+    let name: String
+    let schoolName: String
+    let type: UserScope
+}
---
+++ Target/Source/Domain/Entities/Post.swift
@@ -0,0 +1,15 @@
+import Foundation
+
+struct Post {
+    let author: Author
+    let title: String
+    let content: String
+    let imageUrls: [String]?
+    let createdAt: Date
+    let feedType: PostType
+    let isMine: Bool
+    let isUpdated: Bool
+    let likeCount: Int
+    let viewCount: Int
+    let comments: [Comment]
+}
