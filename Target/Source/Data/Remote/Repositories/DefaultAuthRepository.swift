bd7f234f7f3ef225240c586beb5ecbb9988472ce
baegteun <baegteun@gmail.com>
2022-03-22 11:01:57 +0900

    :sparkles: :: DefaultAuthRepository Implement

---
+++ Target/Source/Data/Remote/Repositories/DefaultAuthRepository.swift
@@ -0,0 +1,30 @@
+import RxSwift
+
+final class DefaultAuthRepository: AuthRepository {
+    private let authRemote = AuthRemote.shared
+    func login(req: LoginRequest, isTest: Bool) -> Completable {
+        authRemote.login(req: req, isTest: isTest)
+    }
+    
+    func sendVerification(req: SendVerificationRequest, isTest: Bool) -> Completable {
+        authRemote.sendVerification(req: req, isTest: isTest)
+    }
+    
+    func checkVerification(req: CheckVerificationRequest, isTest: Bool) -> Completable {
+        authRemote.checkVerification(req: req, isTest: isTest)
+    }
+    
+    func checkIdValidation(id: String, isTest: Bool) -> Completable {
+        authRemote.checkIdValidation(id: id, isTest: isTest)
+    }
+    
+    func findId(email: String, isTest: Bool) -> Single<FindIdResponse> {
+        authRemote.findId(email: email, isTest: isTest)
+    }
+    
+    func changeNewPassword(req: ChangeNewPasswordRequest, isTest: Bool) -> Completable {
+        authRemote.changeNewPassword(req: req, isTest: isTest)
+    }
+    
+    
+}
