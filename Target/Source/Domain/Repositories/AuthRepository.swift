a7841b891f12ded47627d65fbb604e62369cdff1
baegteun <baegteun@gmail.com>
2022-03-22 10:59:27 +0900

    :sparkles: :: AuthRepository Protocol

---
+++ Target/Source/Domain/Repositories/AuthRepository.swift
@@ -0,0 +1,10 @@
+import RxSwift
+
+protocol AuthRepository {
+    func login(req: LoginRequest, isTest: Bool) -> Completable
+    func sendVerification(req: SendVerificationRequest, isTest: Bool) -> Completable
+    func checkVerification(req: CheckVerificationRequest, isTest: Bool) -> Completable
+    func checkIdValidation(id: String, isTest: Bool) -> Completable
+    func findId(email: String, isTest: Bool) -> Single<FindIdResponse>
+    func changeNewPassword(req: ChangeNewPasswordRequest, isTest: Bool) -> Completable
+}
