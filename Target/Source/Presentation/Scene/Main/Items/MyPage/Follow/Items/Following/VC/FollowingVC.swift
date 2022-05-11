c4e88d2d015120f9ac832c3ce2578761eaa853f3
pasin7793 <pasin7793@gmail.com>
2022-05-11 23:04:01 +0900

    :recycle: setup

--- Target/Source/Presentation/Scene/Main/Items/MyPage/Follow/Items/Following/VC/FollowingVC.swift
+++ Target/Source/Presentation/Scene/Main/Items/MyPage/Follow/Items/Following/VC/FollowingVC.swift
@@ -27,8 +27,11 @@ final class MyPageFollowingVC: baseVC<MyPageFollowReactor>{
         $0.showsVerticalScrollIndicator = false
     }
     
-    override func configureVC() {
+    override func setUp() {
         followingListTableView.rx.setDelegate(self).disposed(by: disposeBag)
+    }
+    
+    override func configureVC() {
         view.backgroundColor = MOIZAAsset.moizaGray1.color
     }
     override func addView() {
