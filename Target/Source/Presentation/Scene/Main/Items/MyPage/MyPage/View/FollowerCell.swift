29ccc894a1074334dfb8cec3243ba72e9ea9fc45
baegteun <baegteun@gmail.com>
2022-07-09 18:11:20 +0900

    :recycle: :: following cell

--- Target/Source/Presentation/Scene/Main/Items/MyPage/MyPage/View/FollowerCell.swift
+++ Target/Source/Presentation/Scene/Main/Items/MyPage/MyPage/View/FollowerCell.swift
@@ -22,15 +22,18 @@ final class FollowerCell: baseTableViewCell<FollowerUserList> {
     private let schoolLabel = UILabel().then {
         $0.textColor = MOIZAAsset.moizaGray6.color
         $0.font = UIFont(font: MOIZAFontFamily.Roboto.regular, size: 12)
+        $0.lineBreakMode = .byTruncatingMiddle
     }
     
     private let isFollowButton = UIButton().then {
+        $0.setTitle("맞팔로우", for: .normal)
         $0.backgroundColor = MOIZAAsset.moizaPrimaryBlue.color
         $0.layer.cornerRadius = 5
         $0.setTitleColor(MOIZAAsset.moizaGray1.color, for: .normal)
         $0.titleLabel?.font = UIFont(font: MOIZAFontFamily.Roboto.regular, size: 10)
     }
     private let deleteButton = UIButton().then {
+        $0.setTitle("삭제", for: .normal)
         $0.backgroundColor = MOIZAAsset.moizaGray1.color
         $0.layer.cornerRadius = 5
         $0.layer.borderColor = MOIZAAsset.moizaGray3.color.cgColor
@@ -47,8 +50,6 @@ final class FollowerCell: baseTableViewCell<FollowerUserList> {
         model = nil
         userProfileImageView.image = nil
         schoolLabel.text = nil
-        isFollowButton.setTitle("맞팔로우", for: .normal)
-        deleteButton.setTitle("삭제", for: .normal)
     }
     // MARK: - UI
     override func addView() {
@@ -59,20 +60,17 @@ final class FollowerCell: baseTableViewCell<FollowerUserList> {
         view.flex.layout()
     }
     override func setLayout() {
-        view.flex.direction(.row).marginVertical(5).alignItems(.center).define { flex in
-            flex.addItem(userProfileImageView).marginLeft(4).size(60)
+        view.flex.direction(.row).marginVertical(10).alignItems(.center).define { flex in
+            flex.addItem(userProfileImageView).marginLeft(4).size(55)
             
-            flex.addItem().direction(.column).marginLeft(16).grow(1).define { flex in
-                flex.addItem(userIdLabel).width(45)
-                flex.addItem(schoolLabel).width(100%).marginTop(4)
-            }
-            
-            flex.addItem().direction(.row).paddingLeft(10).grow(1).define { flex in
-                flex.addItem().shrink(1).grow(1).alignItems(.center).define { flex in
-                    flex.addItem(isFollowButton).width(50).height(27).marginRight(10)
+            flex.addItem().direction(.row).grow(1).define { flex in
+                flex.addItem().marginLeft(16).maxWidth(40%).define { flex in
+                    flex.addItem(userIdLabel)
+                    flex.addItem(schoolLabel).marginTop(4)
                 }
-                flex.addItem().shrink(1).grow(1).alignItems(.end).define { flex in
-                    flex.addItem(deleteButton).width(50).height(27).marginRight(5)
+                flex.addItem().direction(.rowReverse).grow(1).define { flex in
+                    flex.addItem(deleteButton).width(50).height(27)
+                    flex.addItem(isFollowButton).width(50).height(27).marginRight(10)
                 }
             }
         }
@@ -84,14 +82,8 @@ final class FollowerCell: baseTableViewCell<FollowerUserList> {
         userProfileImageView.backgroundColor = MOIZAAsset.moizaGray3.color
     
         userIdLabel.text = model.name
-        schoolLabel.text = model.school.display
-        isFollowButton.setTitle("맞팔로우", for: .normal)
-        deleteButton.setTitle("삭제", for: .normal)
-        if model.isFollow {
-            isFollowButton.layer.isHidden = false
-        } else {
-            isFollowButton.layer.isHidden = true
-        }
+        schoolLabel.text = "\(model.school.display) \(model.userScope.display)"
+        isFollowButton.isHidden = model.isFollow
     }
     override func configureCell() {
         self.backgroundColor = .clear
