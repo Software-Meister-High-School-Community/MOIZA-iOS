1d970225bb74f626d8953536e59c5a8fc11664ac
baegteun <baegteun@gmail.com>
2022-07-13 12:23:51 +0900

    :memo: :: ci script

--- ci_scripts/ci_tuist.sh
+++ ci_scripts/ci_tuist.sh
@@ -2,6 +2,7 @@
 
 INSTALL_DIR=$PWD curl -Ls https://install.tuist.io | bash
 
-$INSTALL_DIR/tuist fetch
-$INSTALL_DIR/TUIST_TEST=1 tuist generate
+$INSTALL_DIR/rm -rf Podfile.lock
+$INSTALL_DIR/tuist generate
+$INSTALL_DIR/pod install
 
