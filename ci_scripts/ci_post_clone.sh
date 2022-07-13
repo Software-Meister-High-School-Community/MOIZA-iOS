9f5383c935ba58c8c815e926da81634dd485e413
baegteun <baegteun@gmail.com>
2022-07-13 12:46:24 +0900

    :memo: :: install cocoapods

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
 
-sudo gem install cocoapods
+brew install cocoapods
 INSTALL_DIR=$PWD curl -Ls https://install.tuist.io | bash
 
-$INSTALL_DIR/rm -rf Podfile.lock
 $INSTALL_DIR/tuist generate
 $INSTALL_DIR/pod install
 
